24d15657b07b2cda1e84b04da821d6a5fd4bf579
Nick De Tullio <nrd24@cornell.edu>
2014-04-23 18:26:25 -0400

    updated mybot.c calls snorlax and thunderbolt

--- mybot.c
+++ mybot.c
@@ -2,45 +2,49 @@
 
 void __start(int core_id, int num_crashes, unsigned char payload) {
 
-  if (core_id == 3) {
-    while (1) {
-      int i;
-      for (i = 0; i < TAUNT_SIZE; i++) {
-        if (HOME_STATUS->taunt[i] >= 0) {
-	      Call_Jenny(HOME_STATUS->taunt[i]);
+    unsigned char *ptr = HOME_DATA_SEGMENT; // start on even cache line
+
+    unsigned char *opp_ptr = OPPONENT_DATA_SEGMENT;
+
+    if (core_id == 3) {
+      int i = 0;
+      while(1) {
+        int no_snorlaxes = 15;
+        int no_thunderbolts = 30; 
+          if (no_snorlaxes > 0) {
+            Team_Rocket();
+            opp_ptr[i] = SNORLAX;
+            retreat();
+            no_snorlaxes--;
+            i++;
+          }
+          if (no_thunderbolts > 0) {
+            Team_Rocket();
+            opp_ptr[i] = THUNDERBOLT;
+            retreat();
+            no_thunderbolts--;
+            i++;
+          }
+          else { 
+            for (i = 0; i < TAUNT_SIZE; i++) {
+              if (HOME_STATUS->taunt[i] >= 0) {
+                Call_Jenny(HOME_STATUS->taunt[i]);
+              }            
+            }
+         }
+       }
+    }	
+
+    else { 
+   
+      ptr += core_id * (CACHE_LINE/3);
+
+      while (1) {
+        int i;
+        for (i = 0; i < CACHE_LINE/3; i++) {
+          ptr[i] = payload;
         }
-      }
-    }
-  } 
-  if (core_id == 2) {
-    while (1) {
-      int i = 0
-      while (i < 30) {
-        // write snorlax
-      }
-      int i = 0
-      while (i < 30) {
-        // write thunderbolt
-      }
-    }
-  }
-
-
-  else {
-    int *ptr = (int *) (HOME_DATA_SEGMENT); // start on even cache line
-    // using an int pointer to write 4x as quickly
-    int payload_int = payload << 3 | payload << 2 | payload << 1 | payload;
-
-    // each core fills 1/2 of the cache line, spaced out evenly
-    // must divide by four when incrementing by ptr due to increased size
-    ptr += core_id * ((CACHE_LINE / 2) / 4);
-
-    while (1) {
-      int i;
-      for (i = 0; i < CACHE_LINE/8; i++) {
-        ptr[i] = payload_int;
-      }
-      ptr += (2*CACHE_LINE)/4; // only fill even numbered line
-    }
-  }
+        ptr += 2*CACHE_LINE;
+      } 
+   }
 }
