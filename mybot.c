e59730570298ee239e601db012c9d5cc6e1d1737
Yogisha Dixit <yad4@cornell.edu>
2014-04-27 23:45:09 -0400

    Finalized mybot.

--- mybot.c
+++ mybot.c
@@ -6,48 +6,61 @@ void __start(int core_id, int num_crashes, unsigned char payload) {
 
     unsigned char *opp_ptr = OPPONENT_DATA_SEGMENT;
 
-    if (core_id == 3) {
+    // Dedicate one core to defensive/offensive strategies.
+    if (core_id == 3) 
+    {
       int i = 0;
-      while(1) {
+      while(1) 
+      {
         int no_snorlaxes = 15;
         int no_thunderbolts = 30; 
-          if (no_snorlaxes > 0) {
+          if (no_snorlaxes > 0) 
+          {
             Team_Rocket();
             opp_ptr[i] = SNORLAX;
             retreat();
             no_snorlaxes--;
             i++;
           }
-          if (no_thunderbolts > 0) {
+          if (no_thunderbolts > 0) 
+          {
             Team_Rocket();
             opp_ptr[i] = THUNDERBOLT;
             retreat();
             no_thunderbolts--;
             i++;
          }
-         else { 
-            for (i = 0; i < TAUNT_SIZE; i++) {
-              if (HOME_STATUS->taunt[i] >= 0) {
+         else 
+         { 
+            for (i = 0; i < TAUNT_SIZE; i++) 
+            {
+              if (HOME_STATUS->taunt[i] >= 0) 
+              {
                 Call_Jenny(HOME_STATUS->taunt[i]);
               }            
             }
          }
-         if (no_snorlaxes == 0 && no_thunderbolts == 0) {
+         if (no_snorlaxes == 0 && no_thunderbolts == 0) 
+         {
            invalidate(opp_ptr);
          }
-         else {
+         else 
+         {
            prefetch(opp_ptr);
          }
        }
     }	
-
+    // The other three write to our half of memory
     else { 
    
       ptr += core_id * (CACHE_LINE/3)/4;
 
-      while (1) {
+      while (1) 
+      {
         int i;
-        for (i = 0; i < CACHE_LINE/12; i++) {
+        for (i = 0; i < CACHE_LINE/12; i++) 
+        {
+          // Write four at once
           ptr[i] = payload << 24 | payload << 16 | payload << 8 | payload;
         }
         ptr += (2*CACHE_LINE)/4;
